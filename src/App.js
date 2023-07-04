0adbb66abdd39ec037b185837a1e71deced85789
Emmanuel Asabere <kwabenaAsabere05@gmail.com>
2023-07-03 21:41:19 -1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './components/pages/Home';
+import RoadmapPage from './components/pages/RoadmapPage';
+import DataState from './context/data/DataState';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <main className='main-app'>
+        <DataState>
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route exact path='/roadmappage' element={<RoadmapPage/>} />
+          </Routes>
+        </DataState>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
